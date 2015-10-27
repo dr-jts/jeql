587ddfb40a9a2d648501f7f7aea5b22b984caa36
dr-jts <mtnclimb@gmail.com>
2015-10-27 10:49:03 -0700

    Add Select Assist entries

--- modules/jeqlw/src/main/java/jeql/workbench/ui/assist/SelectAssistPanel.java
+++ modules/jeqlw/src/main/java/jeql/workbench/ui/assist/SelectAssistPanel.java
@@ -36,6 +36,15 @@ public class SelectAssistPanel extends Column2AssistPanel
   public void populateList()
   {
     add("select * ");
+    add("select distinct * ");
+    add("select * from _");
+    add("select * from _ where _");
+    add("select * from _ join _ on _");
+    add("select * from _ join _ on _ where _");
+    add("select * from _ join _ on _ where _ group by _");
+    add("select * from _ join _ on _ where _ order by _");
+    add("select * let <assign1>, <assign2> from _ where _");
+    add("select splitIndex, splitValue from _ split by _ where _");
     add("let _ ");
     add("from _ ");
     add("join _ on _ ");
@@ -45,12 +54,6 @@ public class SelectAssistPanel extends Column2AssistPanel
     add("order by _ ");
     add("limit 1");
     add("limit 1 offset 1");
-    add("select * from _");
-    add("select * from _ where _");
-    add("select * from _ join _ on _");
-    add("select * from _ join _ on _ where _");
-    add("select * let <assign1>, <assign2> from _ where _");
-    add("select splitIndex, splitValue from _ split by _ where _");
 
   }
 
