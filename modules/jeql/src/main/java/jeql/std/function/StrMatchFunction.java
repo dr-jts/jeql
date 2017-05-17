bc86f95dc5724cba94de342022c87ffdf3be79a4
Martin Davis <mtnclimb@gmail.com>
2017-05-16 21:51:24 -0700

    Add StrMatch longestCommonString and longestCommonSequence

--- modules/jeql/src/main/java/jeql/std/function/StrMatchFunction.java
+++ modules/jeql/src/main/java/jeql/std/function/StrMatchFunction.java
@@ -2,74 +2,154 @@ package jeql.std.function;
 
 import jeql.api.function.FunctionClass;
 
-public class StrMatchFunction
-implements FunctionClass
-{
-  public static int levenshteinDistance (String s, String t) {
-    if (s == null || t == null) {
-      throw new IllegalArgumentException("Strings must not be null");
-    }
-      
-    /*
-      The difference between this impl. and the previous is that, rather 
-       than creating and retaining a matrix of size s.length()+1 by t.length()+1, 
-       we maintain two single-dimensional arrays of length s.length()+1.  The first, d,
-       is the 'current working' distance array that maintains the newest distance cost
-       counts as we iterate through the characters of String s.  Each time we increment
-       the index of String t we are comparing, d is copied to p, the second int[].  Doing so
-       allows us to retain the previous cost counts as required by the algorithm (taking 
-       the minimum of the cost count to the left, up one, and diagonally up and to the left
-       of the current cost count being calculated).  (Note that the arrays aren't really 
-       copied anymore, just switched...this is clearly much better than cloning an array 
-       or doing a System.arraycopy() each time  through the outer loop.)
+public class StrMatchFunction implements FunctionClass {
+	public static int levenshteinDistance(String s, String t) {
+		if (s == null)
+			s = "";
+		if (t == null)
+			t = "";
+		/*
+		 * if (s == null || t == null) { throw new IllegalArgumentException(
+		 * "Strings must not be null"); }
+		 * 
+		 */
 
-       Effectively, the difference between the two implementations is this one does not 
-       cause an out of memory condition when calculating the LD over two very large strings.      
-    */    
-      
-    int n = s.length(); // length of s
-    int m = t.length(); // length of t
-      
-    if (n == 0) {
-      return m;
-    } else if (m == 0) {
-      return n;
-    }
+		/*
+		 * The difference between this impl. and the previous is that, rather
+		 * than creating and retaining a matrix of size s.length()+1 by
+		 * t.length()+1, we maintain two single-dimensional arrays of length
+		 * s.length()+1. The first, d, is the 'current working' distance array
+		 * that maintains the newest distance cost counts as we iterate through
+		 * the characters of String s. Each time we increment the index of
+		 * String t we are comparing, d is copied to p, the second int[]. Doing
+		 * so allows us to retain the previous cost counts as required by the
+		 * algorithm (taking the minimum of the cost count to the left, up one,
+		 * and diagonally up and to the left of the current cost count being
+		 * calculated). (Note that the arrays aren't really copied anymore, just
+		 * switched...this is clearly much better than cloning an array or doing
+		 * a System.arraycopy() each time through the outer loop.)
+		 * 
+		 * Effectively, the difference between the two implementations is this
+		 * one does not cause an out of memory condition when calculating the LD
+		 * over two very large strings.
+		 */
 
-    int p[] = new int[n+1]; //'previous' cost array, horizontally
-    int d[] = new int[n+1]; // cost array, horizontally
-    int _d[]; //placeholder to assist in swapping p and d
+		int n = s.length(); // length of s
+		int m = t.length(); // length of t
 
-    // indexes into strings s and t
-    int i; // iterates through s
-    int j; // iterates through t
+		if (n == 0) {
+			return m;
+		} else if (m == 0) {
+			return n;
+		}
 
-    char t_j; // jth character of t
+		int p[] = new int[n + 1]; // 'previous' cost array, horizontally
+		int d[] = new int[n + 1]; // cost array, horizontally
+		int _d[]; // placeholder to assist in swapping p and d
 
-    int cost; // cost
+		// indexes into strings s and t
+		int i; // iterates through s
+		int j; // iterates through t
 
-    for (i = 0; i<=n; i++) {
-       p[i] = i;
-    }
-      
-    for (j = 1; j<=m; j++) {
-       t_j = t.charAt(j-1);
-       d[0] = j;
-      
-       for (i=1; i<=n; i++) {
-          cost = s.charAt(i-1)==t_j ? 0 : 1;
-          // minimum of cell to the left+1, to the top+1, diagonally left and up +cost        
-          d[i] = Math.min(Math.min(d[i-1]+1, p[i]+1),  p[i-1]+cost);  
-       }
+		char t_j; // jth character of t
 
-       // copy current distance counts to 'previous row' distance counts
-       _d = p;
-       p = d;
-       d = _d;
-    } 
-      
-    // our last action in the above loop was to switch d and p, so p now 
-    // actually has the most recent cost counts
-    return p[n];
-  }
+		int cost; // cost
+
+		for (i = 0; i <= n; i++) {
+			p[i] = i;
+		}
+
+		for (j = 1; j <= m; j++) {
+			t_j = t.charAt(j - 1);
+			d[0] = j;
+
+			for (i = 1; i <= n; i++) {
+				cost = s.charAt(i - 1) == t_j ? 0 : 1;
+				// minimum of cell to the left+1, to the top+1, diagonally left
+				// and up +cost
+				d[i] = Math.min(Math.min(d[i - 1] + 1, p[i] + 1), p[i - 1] + cost);
+			}
+
+			// copy current distance counts to 'previous row' distance counts
+			_d = p;
+			p = d;
+			d = _d;
+		}
+
+		// our last action in the above loop was to switch d and p, so p now
+		// actually has the most recent cost counts
+		return p[n];
+	}
+
+	public static String longestCommonString(String str1, String str2) {
+
+		StringBuilder sb = new StringBuilder();
+		if (str1 == null || str1.isEmpty() || str2 == null || str2.isEmpty())
+			return "";
+
+		// java initializes array with 0s
+		int[][] num = new int[str1.length()][str2.length()];
+		int maxlen = 0;
+		int lastSubsBegin = 0;
+
+		for (int i = 0; i < str1.length(); i++) {
+			for (int j = 0; j < str2.length(); j++) {
+				if (str1.charAt(i) == str2.charAt(j)) {
+					if ((i == 0) || (j == 0))
+						num[i][j] = 1;
+					else
+						num[i][j] = 1 + num[i - 1][j - 1];
+
+					if (num[i][j] > maxlen) {
+						maxlen = num[i][j];
+						// generate substring from str1 => i
+						int thisSubsBegin = i - num[i][j] + 1;
+						if (lastSubsBegin == thisSubsBegin) {
+							// if the current LCS is the same as the last time
+							// this block ran
+							sb.append(str1.charAt(i));
+						} else {
+							// this block resets the string builder if a
+							// different LCS is found
+							lastSubsBegin = thisSubsBegin;
+							sb = new StringBuilder();
+							sb.append(str1.substring(lastSubsBegin, i + 1));
+						}
+					}
+				}
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static String longestCommonSequence(String a, String b) {
+		int[][] lengths = new int[a.length() + 1][b.length() + 1];
+
+		// row 0 and column 0 are initialized to 0 already
+
+		for (int i = 0; i < a.length(); i++)
+			for (int j = 0; j < b.length(); j++)
+				if (a.charAt(i) == b.charAt(j))
+					lengths[i + 1][j + 1] = lengths[i][j] + 1;
+				else
+					lengths[i + 1][j + 1] = Math.max(lengths[i + 1][j], lengths[i][j + 1]);
+
+		// read the substring out from the matrix
+		StringBuffer sb = new StringBuffer();
+		for (int x = a.length(), y = b.length(); x != 0 && y != 0;) {
+			if (lengths[x][y] == lengths[x - 1][y])
+				x--;
+			else if (lengths[x][y] == lengths[x][y - 1])
+				y--;
+			else {
+				assert a.charAt(x - 1) == b.charAt(y - 1);
+				sb.append(a.charAt(x - 1));
+				x--;
+				y--;
+			}
+		}
+
+		return sb.reverse().toString();
+	}
 }
