e10b8da75483dbe9432c1a1029df00d079df90af
Martin Davis <mtnclimb@gmail.com>
2019-01-17 09:55:28 -0800

    Add hilbertPoint function and demo

--- modules/jeql/src/main/java/jeql/std/function/HashFunction.java
+++ modules/jeql/src/main/java/jeql/std/function/HashFunction.java
@@ -1,6 +1,10 @@
 package jeql.std.function;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Point;
+
 import jeql.api.function.FunctionClass;
+import jeql.std.geom.GeomFunction;
 
 public class HashFunction implements FunctionClass {
   
@@ -70,4 +74,62 @@ public class HashFunction implements FunctionClass {
     long index = ((i1 << 1) | i0) >> (32 - 2 * ord);
     return (int) index;
   }
+  
+  public static Point hilbertPoint(int order, int i) {
+    // Fast Hilbert curve algorithm by http://threadlocalmutex.com/
+    // Ported from C++ https://github.com/rawrunprotected/hilbert_curves (public
+    // domain)
+    
+    int ord = hilbertOrderClamp(order);
+    
+    i = i << (32 - 2 * ord);
+
+    long i0 = deinterleave(i);
+    long i1 = deinterleave(i >> 1);
+
+    long t0 = (i0 | i1) ^ 0xFFFF;
+    long t1 = i0 & i1;
+
+    long prefixT0 = prefixScan(t0);
+    long prefixT1 = prefixScan(t1);
+
+    long a = (((i0 ^ 0xFFFF) & prefixT1) | (i0 & prefixT0));
+
+    long x = (a ^ i1) >> (16 - ord);
+    long y = (a ^ i0 ^ i1) >> (16 - ord);
+    
+    return GeomFunction.geomFactory.createPoint(new Coordinate(x, y));
+  }
+
+  private static long prefixScan(long x) {
+    x = (x >> 8) ^ x;
+    x = (x >> 4) ^ x;
+    x = (x >> 2) ^ x;
+    x = (x >> 1) ^ x;
+    return x;
+  }
+
+  private static long deinterleave(int x) {
+    x = x & 0x55555555;
+    x = (x | (x >> 1)) & 0x33333333;
+    x = (x | (x >> 2)) & 0x0F0F0F0F;
+    x = (x | (x >> 4)) & 0x00FF00FF;
+    x = (x | (x >> 8)) & 0x0000FFFF;
+    return x;
+  }
+  
+  /**
+   * Clamps an order to the range valid for 
+   * the index algorithm used.
+   * 
+   * @param order the order of a Hilbert curve
+   * @return a valid order
+   */
+  private static int hilbertOrderClamp(int order) {
+    // clamp order to [1, 16]
+    int ord = order < 1 ? 1 : order;
+    ord = ord > 16 ? 16 : ord;
+    return ord;
+  }
+
 }
